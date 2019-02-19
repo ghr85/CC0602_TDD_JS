82c7c3ac33aaf57ff5925b1dca03431f2ab995fd
ghr85 <ghrenfrew@gmail.com>
2019-02-19 16:02:55 +0000

    added stock calculation to decorator object

--- decorator.js
+++ decorator.js
@@ -2,7 +2,7 @@
 //TDD in Javascript
 //Painter/Decorator
 //Decorator
-
+require('./paint_can.js')
 const Decorator = function(){
 this.stock = [];
 };
@@ -11,4 +11,14 @@ this.stock = [];
 Decorator.prototype.get_paint = function(paint_can){
   this.stock.push(paint_can);
 };
+
+
+Decorator.prototype.stock_count = function(){
+let litre_count = 0;
+for(let currentCan of this.stock){
+  litre_count += currentCan.litres;
+};
+return litre_count;
+};
+
 module.exports = Decorator;

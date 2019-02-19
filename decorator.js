cf9db034ac4baa2dee33ad5f536cea9e8b995c56
ghr85 <ghrenfrew@gmail.com>
2019-02-19 15:53:53 +0000

    added total litres of paint calculation test to decorator spec

--- decorator.js
+++ decorator.js
@@ -1,8 +1,14 @@
+//Codeclan Week 06 Day 02
+//TDD in Javascript
+//Painter/Decorator
+//Decorator
+
 const Decorator = function(){
 this.stock = [];
 };
 
-// Room.prototype.paint_room = function(){
-//   this.painted = true;
-// };
+//remember parameter is in function parameters, not prototype declaration
+Decorator.prototype.get_paint = function(paint_can){
+  this.stock.push(paint_can);
+};
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -6,17 +6,32 @@
 
 const assert = require('assert'); //require Node's assertion module
 const Decorator = require('../decorator.js');
+const Paint_can = require('../paint_can.js');
 
 describe('Decorator', function(){ //bundle tests together 1st arg = label, 2nd arg anon funct
   let decorator;//note scope assignment being passed to child functions
   beforeEach(function(){
      decorator = new Decorator();
+     paint_can = new Paint_can(5);
   })
 
  it('should start with an empty paint stock',function(){
    const actual = decorator.stock.length;
    const expected = 0;
    assert.strictEqual(actual,expected);
- });//describe properties the taxi should have
+ });
 
+ it('should be able to add a can of paint to stock',function(){
+   decorator.get_paint(paint_can);
+   const actual = decorator.stock.length;
+   const expected = 1;
+   assert.strictEqual(actual,expected);
+ });
+ it('should be able to calculate stock',function(){
+   decorator.get_paint(paint_can);
+   decorator.get_paint(paint_can);
+   const actual = decorator.stock_count();
+   const expected = 10;
+   assert.strictEqual(actual,expected);
+ });
 });
